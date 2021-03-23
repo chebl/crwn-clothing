f84873a66aabd255ae2be844b5ed513cd6b8677f
chebl <chebl.mahmoud@gmail.com>
2021-03-23 13:33:22 +0100

    implemented firebase utils,included ability to store authenticated users into our firestore database

--- src/App.js
+++ src/App.js
@@ -5,43 +5,60 @@ import HomePage from './pages/homepage/homepage.component';
 import ShopPage from './pages/shop/shop.component';
 import SignInAndSignUpPage from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component';
 import Header from './components/header/header.component';
-import { auth } from './firebase/firebase.utils';
+import { auth, createUserProfileDocument } from './firebase/firebase.utils';
 import React from 'react';
 
 
 class App extends React.Component {
-  constructor(){
+  constructor() {
     super();
 
     this.state = {
       currentUser: null
     }
-  } 
-  unsbscribeFromAuth=null;
-
-  componentDidMount(){
-    this.unsbscribeFromAuth=auth.onAuthStateChanged(user =>{
-   this.setState({currentUser:user});
-   console.log(user); 
-   }); 
   }
- 
+  unsbscribeFromAuth = null;
+
+  componentDidMount() {
+    this.unsbscribeFromAuth = auth.onAuthStateChanged(async userAuth => {
+      if (userAuth) {
+        const userRef = await createUserProfileDocument(userAuth);
+        userRef.onSnapshot(snapShot => {
+          this.setState({
+            currentUser: {
+              id: snapShot.id,
+              ...snapShot.data()
+            }
+          },
+          () => {
+            console.log(this.state);
+          }
+          );
+
+        });
+      }
+      else {
+        this.setState({ currentUser: userAuth });
+      }
+    });
+  }
+
   componentWillUnmount() {
     this.unsbscribeFromAuth();
   }
   render() {
-  return (
-    <div>
-      <Header currentUser={this.state.currentUser} />
-      <Switch>
-        <Route exact path='/' component={HomePage} />
-        <Route exact path='/shop' component={ShopPage} />
-        <Route exact path='/signin' component={SignInAndSignUpPage} />
-        
-      </Switch>
-    </div>
-  );
+    return (
+      <div>
+        <Header currentUser={this.state.currentUser} />
+        <Switch>
+          <Route exact path='/' component={HomePage} />
+          <Route exact path='/shop' component={ShopPage} />
+          <Route exact path='/signin' component={SignInAndSignUpPage} />
 
- }
+        </Switch>
+      </div>
+    );
+
+  }
 }
 export default App;
